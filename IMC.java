1f739848d35310908e112df915a788e39d66431d
nathaliasn <nathaliasemann@gmail.com>
2018-09-05 21:35:26 -0300

    First commit

---
+++ IMC.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+public class IMC
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        float peso,
+              altura,
+              imc; 
+ 
+     System.out.print("Informe seu peso? "); 
+     peso = input.nextFloat();
+
+     System.out.print("Informe sua altura? "); 
+     altura = input.nextFloat();
+
+     imc = (peso / (altura * altura));
+     System.out.printf("seu IMC \u00e9 %.1f", imc);
+
+    }
+}
+
+      
